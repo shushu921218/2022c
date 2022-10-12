f8ec753333dab024f116d92cf6df0a02998a47fb
shushu921218 <114201484+shushu921218@users.noreply.github.com>
2022-10-13 02:15:23 +0800

    week05

---
+++ week05-3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    ///int a=10;
+    ///while (a>0) {
+    ///    printf("a:%d a>0\n",a);
+    ///    a--;
+    ///}
+    for(int a=10 ; a>0 ; a--){
+        printf("a:%d a>0\n",a);
+    }
+}
